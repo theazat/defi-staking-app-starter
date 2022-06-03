002dac40fce5cd9065d46d3bc8985a8ab36b9977
Azat Sayan <sayanazat@gmail.com>
2022-06-03 17:00:20 +0300

    Smart Contract Transactions between each other

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -2,8 +2,22 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock Tether Contract
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
+
+  // Deploy RWD contract
   await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
+
+  // Deploy DecentralBank contract
   await deployer.deploy(DecentralBank);
+  const decentralBank = DecentralBank.deployed();
+
+  // Transfer all RWD tokens to Decentral Bank
+  await rwd.transfer(DecentralBank.address, "1000000000000000000000000");
+
+  // Distribute 100 Tether tokens to investor
+  await tether.transfer(accounts[1], "1000000000000000000");
 };
