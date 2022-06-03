76170522440928698a70e50d5c7ea5b7489b5267
Azat Sayan <sayanazat@gmail.com>
2022-06-04 00:43:50 +0300

    Assertion & Mocha/Chai Tests

---
+++ defi-staking-app-starter/test/decentralBank.tests.js
@@ -0,0 +1,42 @@
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("DecentralBank", ([owner, customer]) => {
+  let tether, rwd, decentralBank;
+
+  function tokens(number) {
+    return web3.utils.toWei(number, "ether");
+  }
+
+  before(async () => {
+    // Load contracts
+    tether = await Tether.new();
+    rwd = await RWD.new();
+    decentralBank = await DecentralBank.new(rwd.address, tether.address);
+
+    // Transfer all tokens to Decentral Bank (1 million)
+    await rwd.transfer(decentralBank.address, tokens("1000000"));
+
+    // Transfer 100 mock Tethers to customer
+    await tether.transfer(customer, tokens("100"), { from: owner });
+  });
+
+  describe("Mock Tether Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await tether.name();
+      assert.equal(name, "Mock Tether Token");
+    });
+  });
+
+  describe("Reward Token", async () => {
+    it("matches name successfully", async () => {
+      const name = await rwd.name();
+      assert.equal(name, "Reward Token");
+    });
+  });
+});
