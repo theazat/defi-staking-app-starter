67e8362f467e183bc661ed2d1872ba34f74f1267
Azat Sayan <sayanazat@gmail.com>
2022-06-05 13:11:51 +0300

    Deposit tokens functionality

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -9,8 +9,23 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    // staking function
+    function depositToken(uint256 _amount) public {
+        // Transfer tether tokens to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        // Uodate Staking Balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
     }
 }
