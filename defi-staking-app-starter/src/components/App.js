982e03a1d77dc4d9646360d1778098af71846983
Azat Sayan <sayanazat@gmail.com>
2022-06-06 15:59:46 +0300

    Building Navnar UI

---
+++ defi-staking-app-starter/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="text-center">
+        <h>Hello World </h>
+      </div>
+    </div>
+  );
+};
+
+export default App;
